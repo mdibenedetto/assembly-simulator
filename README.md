9c9c508281f096c5dfc97c22ab59b7e6e71d9469
michele dibenedetto <michele.dibe78@gmail.com>
2020-03-31 17:05:22 +0100

    code rafactoring

---
+++ README.md
@@ -0,0 +1,2 @@
+ you can asses by using this link
+ [NCI-Assembly-simulator](https://mdibenedetto.github.io/NCI-Assembly-simulator/).
---
+++ js/common.js
@@ -0,0 +1,17 @@
+function loadNavbar() {
+  const navBar = `  
+  <nav>
+    <a href="./">
+    Home
+    </a>
+    |
+    <a href="./traffic-lights.html">
+      Traffic lights
+    </a>
+    |
+    <a href="./seven-segments-led.html">
+      Seven segments led
+    </a>
+  </nav>`;
+  document.write(navBar);
+}
